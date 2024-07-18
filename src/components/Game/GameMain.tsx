d039469cce6583ebc231ab4ab1239612acf2cdec
Michael <michaellngriffin@gmail.com>
2024-07-18 14:07:42 -0400

    added mockup of game ui

---
+++ src/components/Game/GameMain.tsx
@@ -0,0 +1,43 @@
+/**
+ * Game is the main hub after a deck has been selected.
+ * Component Flow
+ *
+ * App -> Game -> Stage/Round/Shop
+ */
+
+import './GameMain.css'
+
+function GameMain(){
+
+  return (
+    <div className="fullbody">
+      <div className="SideMenu"></div>
+      <main>
+        <div className="JokerSection"></div>
+        <div className="ConsumableSection"></div>
+        <div className="StageSelect"></div> {/* Switch out with Round or Shop */}
+        <div className="DeckSection"></div>
+      </main>
+    </div>
+  )
+}
+
+
+export default GameMain;
+
+
+// return (
+//   <div className="fullbody">
+//     <SideMenu />
+//     <main>
+//       <div className="top-section">
+//         <JokerSection />
+//         <ConsumableSection />
+//       </div>
+//       <div className="bottom-section">
+//         <StageSelect /> {/* Switch out with Round or Shop */}
+//         <DeckSection />
+//       </div>
+//     </main>
+//   </div>
+// )
