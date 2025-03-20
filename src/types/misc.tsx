8b97e394455b4f35040ab30d1d7cdd240862a9ed
Michael <michaellngriffin@gmail.com>
2025-03-20 18:45:33 -0400

    renamed types file

--- src/types/misc.tsx
+++ src/types/misc.tsx
@@ -1,4 +1,5 @@
-type RankNum = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13
+export type RankNum = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+export type Suits = 'clubs' | 'diamonds' | 'hearts' | 'spades';
 type Enhancements = "bonus" | "mult" | "wild" | "glass" | "steel" |
   "stone" | "gold" | "lucky";
 type SpecialCardMods = "foil" | "holo" | "poly";
@@ -7,11 +8,11 @@ type Seals = "red" | "blue" | "purple" | "gold";
 
 export type Card = {
   rank: RankNum,
-  suit: 'clubs' | 'diamonds' | 'hearts' | 'spades' //string,
-  type: 'ace' | 'face' | 'number' | 'stone'
-  enhanced: Enhancements | null, //
+  type: 'ace' | 'face' | 'number' | 'stone',
+  suit: 'clubs' | 'diamonds' | 'hearts' | 'spades',
+  enhanced: Enhancements | null,
   special: SpecialCardMods | null,
-  seal: Seals | null
+  seal: Seals | null,
 }
 
 export type CardComponent = Card & {id: number, selected: boolean};
