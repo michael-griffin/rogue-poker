163d7bda6ab7484c5de71bc16cdd68fee41185c7
Michael <michaellngriffin@gmail.com>
2025-06-10 20:14:13 -0400

    added outline for tarot/planet tests

---
+++ src/tests/tarots.test.tsx
@@ -0,0 +1,96 @@
+// export type TarotTypes = 'moon'|'star'|'sun'|'world'|  //suit change for 3
+//   'magician'|'empress'|'hierophant'|'strength'| //enhance 2
+//   'lovers'|'chariot'|'justice'|'devil'|'tower'| //enhance 1
+//   'fool'|'highPriestess'|'emperor'|'judgment'|  //create misc
+//   'wheelOfFortune'|'hermit'|'temperance'|       //bonus
+//   'death'|'hangedMan';                          //copy/destroy 2
+
+
+describe('test suit change functions', function(){
+  const deckOfSpades = [];
+  const deckOfHearts = [];
+  //deal hand, select first 3 cards
+  test('test moon (clubs)', function(){
+    //
+
+    //confirm dealtCards has shifted?
+    //confirm unselect?
+    //select 5, play hand, ensure it's not a flush?
+  })
+  test('test star (diamonds)', function(){
+
+  })
+  test('test sun (hearts)', function(){
+
+  })
+  test('test moon (spades)', function(){
+
+  })
+})
+
+
+
+describe('test enhanced 2 tarots', function(){
+  test('test magician (lucky)', function(){
+    //
+
+    //confirm dealtCards has shifted (check enhanced)
+    //confirm unselect
+    //select 5, play hand, ensure it's not a flush?
+  })
+  test('test empress (mult)', function(){
+
+  })
+  test('test hierophant (chip)', function(){
+
+  })
+  test('test strength (rank up)', function(){
+
+    //check rank before+after
+    //see if a pair can be made when playing hand
+  })
+})
+
+
+//   'lovers'|'chariot'|'justice'|'devil'|'tower'| //enhance 1
+describe('test enhance 1 tarots', function(){
+  test('test lovers (wild)', function(){
+    //
+
+    //confirm dealtCards has shifted (check enhanced)
+    //confirm unselect
+    //select 5, play hand, ensure there is now a flush?
+  })
+  test('test chariot (steel)', function(){
+
+  })
+  test('test justice (glass)', function(){
+
+  })
+  test('test devil (gold)', function(){
+
+    //check rank before+after
+    //see if a pair can be made when playing hand
+  })
+  test('test tower (stone)', function(){
+
+  })
+})
+
+
+
+describe('test modify 2 tarots', function(){
+  test('test death (copy)', function(){
+
+  })
+  test('test hanged man (destroy)', function(){
+
+  })
+})
+
+
+//   'fool'|'highPriestess'|'emperor'|'judgment'|  //create misc
+
+
+
+//   'wheelOfFortune'|'hermit'|'temperance'|       //bonus
